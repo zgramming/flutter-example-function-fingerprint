5e2dfa0fe5ece7fed2c53dfbb8832ca77116ff2d
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-01-30 14:03:14 +0700

    Final Improve

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/error_codes.dart';
 
-void main() => runApp(InitialPage());
+void main() => runApp(
+      InitialPage(),
+    );
 
 class InitialPage extends StatelessWidget {
   @override
@@ -17,8 +22,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  LocalAuthentication localAuthentication = LocalAuthentication();
+  bool _isValid = false;
+  Future<void> authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await localAuthentication.authenticateWithBiometrics(
+        localizedReason: 'Scan your fingerprint to authenticate',
+        useErrorDialogs: true,
+        stickyAuth: true,
+      );
+      if (authenticated) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => WelcomePage(),
+          ),
+        );
+        _isValid = true;
+      } else {
+        print("Failed");
+        setState(() => _isValid = false);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    Future.delayed(Duration(milliseconds: 500), () {
+      authenticate();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fingerprint Example'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.fingerprint,
+                size: 80,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              child: Text('Provided Your Fingerprint Scan'),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: _isValid
+                ? SizedBox()
+                : Align(
+                    alignment: Alignment.bottomCenter,
+                    child: RaisedButton(
+                      color: Theme.of(context).errorColor,
+                      onPressed: authenticate,
+                      child: Text('Try Again'),
+                      textTheme: ButtonTextTheme.primary,
+                    ),
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome Page'),
+      ),
+    );
   }
 }
